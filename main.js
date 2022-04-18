d3d9839e28c44c4bdd9cc566912fa40bca3d1c89
QunicyAternos <93652053+QunicyAternos@users.noreply.github.com>
2022-04-18 17:44:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized!')
+}
+
+function draw()
+{
+    background('#A2F3FE');
+    fill('#A2FECF');
+    stroke('#A2FECF');
+    textSize(difference);
+    text('valhalla',50,400);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX =" + leftWristX +" rightWristX =" + rightWristX + " difference" + difference);
+    }
+}
